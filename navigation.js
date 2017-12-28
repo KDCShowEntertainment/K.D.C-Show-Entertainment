01cd886907ed02eceae2091758b88b2b22faecfa
Michael Odumosu <michaelodumosu57@gmail.com>
2017-12-28 13:11:54 -0500

    Undone work 11/28/17 -finsihed up nav activation now to work on nav deactivation animating navigation select by choose any with navigator, further restricts if one is already chosen

--- navigation.js
+++ navigation.js
@@ -7,12 +7,14 @@
             }
             
 
+// you can bind each navigator item to an event and trigger the event in appropriate timing to aviod the recusion
 
+        var nav_info = ["Home","Calendar","Contact Us","Venues","Amenities","Cast","Tickets"]
                 $(document).ready(function() {
                     var nav_card = $(".navigator").clone();
 
                     $(".nav_container").append(nav_card);
-                    var i = 3;
+                    var i = 5;
                     while(i != 0){
                         $(".nav_container").append(nav_card.clone());
                         i -= 1;
@@ -20,20 +22,33 @@
                     }
                     var navigator_set = $.map($(".navigator"),function (link_item,index){
                         $(".navigator > h3").eq(index).css({"text-align":"center","margin-top":"-.3em"})
+                        
+                        $(".navigator > h3").eq(index).text(nav_info[index])
                         return $(link_item)
                     })
                     
                     i = 0;
+                    j = 0;
+                    interval_count = 0;
                     function nav_by_one(){
+                        interval_count += 1;
                         if(i == navigator_set.length){
                             
                             return;
                         }
                         else{
-                            i += 1;
-                            navigator_set[i -1].animate({
-                                left:"18em"
-                            },700,nav_by_one)
+                            i+= 1;
+                            console.log("animating",i)
+                            
+                            if (interval_count > i){
+                                i = interval_count;
+                            }
+                            console.log(interval_count,i)
+                            
+                                navigator_set[i - 1 ].animate({
+                                    left:"18em"
+                                },700,nav_by_one)
+                            
                             
                             
                         }
@@ -45,27 +60,41 @@
 
                     
                     $(".nav_container").css("opacity",".7")
+                    
                     $(".nav_header").css("opacity","0")
                     
                     
                     i = 0
-                    $(".nav_container").one("click mouseover",function () {
-                        
+                    
+                    // $(".navigator").on("hit", function(){
+                    //     $(this).animate({
+                    //         left:"18em"
+                    //     })
+                    // })
+                    var executed = false;
+                    $(".navigator").one("click mouseover",function () {
+                        if (executed == true){
+                            return
+                        }
+                        executed = true;
                         $("body > :not(.navigator)").css("opacity",".3")
                         // $("div:not(.navigator)").css("opacity",".3")
                         // $(".home_section").css("opacity",".3")
                         
                         $("body").css("opacity","1")
+                        $(".nav_container").css({"opacity":"1","height":"30em","overflow-y":"scroll","overflow-x":"hidden"})
                         $(this).css("opacity","1")
-                        $(".navigator").css("opacity","1")
                         $(".nav_header").css("opacity","1")
                         nav_by_one();
+                        // navigator_set.forEach(function (show_up) {
+                        //     show_up.trigger("hit");
+                        // })
                         i = 0
                         
 
                         
                     })
-                    // animating navigation
+                    // animating navigation select by choose any with navigator, further restricts if one is already chosen
                     
                     
                 });
